94682627953214efa9425b1f7b93b7996c142a34
skophek <skophek@lipe.cz>
2021-03-14 18:47:29 +0000

    init

---
+++ src/components/RemoteControl/RemoteControl.js
@@ -0,0 +1,14 @@
+import './RemoteControl.scss';
+import React, ***REMOVED*** Component ***REMOVED*** from 'react'
+
+class RemoteControl extends Component ***REMOVED***
+    render () ***REMOVED***
+        return (
+            <div className="RemoteControl">
+                
+            </div>
+        )
+  ***REMOVED***
+***REMOVED***
+
+export default RemoteControl;
