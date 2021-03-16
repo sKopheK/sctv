fbb28474a83938f91c022574272c11c856cf5417
skophek <skophek@lipe.cz>
2021-03-16 17:23:43 +0000

    AppCtx to share state: - main switch button toggles TV

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, ***REMOVED*** useContext ***REMOVED*** from 'react';
+import AppCtx from '../../AppCtx';
 import './Overlay.scss';
 
-const Overlay = (***REMOVED*** className ***REMOVED***) => (
-  <div className=***REMOVED***`Overlay $***REMOVED***className ?? ''***REMOVED***`***REMOVED*** />
-);
+const Overlay = (***REMOVED*** className ***REMOVED***) => ***REMOVED***
+  const ***REMOVED*** screenOn ***REMOVED*** = useContext(AppCtx);
+  return (
+    <div className=***REMOVED***`Overlay $***REMOVED***screenOn ? 'no-signal' : ''***REMOVED*** $***REMOVED***className ?? ''***REMOVED***`***REMOVED*** />
+  );
+***REMOVED***;
 
 Overlay.propTypes = ***REMOVED***
   className: PropTypes.string,
