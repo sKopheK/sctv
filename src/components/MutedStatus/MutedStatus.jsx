2c1de8e5d97b8e0f487ec563aa3421f1f2693e83
skophek <skophek@lipe.cz>
2021-03-26 23:45:56 +0000

    MutedStatus panel

---
+++ src/components/MutedStatus/MutedStatus.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MutedStatus.scss';
+
+function MutedStatus() ***REMOVED***
+  return (
+    <div className="MutedStatus">
+      <span className="xico-cross" />
+      Muted
+    </div>
+  );
+***REMOVED***
+
+export default MutedStatus;
