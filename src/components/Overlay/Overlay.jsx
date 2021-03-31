3529053cadcb7dd4e60639114b741984222871dd
skophek <skophek@lipe.cz>
2021-03-31 19:32:27 +0100

    render optimization

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
-import React, ***REMOVED*** useContext ***REMOVED*** from 'react';
+import React, ***REMOVED*** useContext, useMemo ***REMOVED*** from 'react';
 import AppCtx from '../../state/AppCtx';
 import './Overlay.scss';
 
 const Overlay = (***REMOVED*** className ***REMOVED***) => ***REMOVED***
   const ***REMOVED*** screenOn ***REMOVED*** = useContext(AppCtx);
-  return (
+  return useMemo(() => (
     <div className=***REMOVED***`Overlay $***REMOVED***screenOn ? 'no-signalx' : ''***REMOVED*** $***REMOVED***className ?? ''***REMOVED***`***REMOVED*** />
-  );
+  ), [screenOn, className]);
 ***REMOVED***;
 
 Overlay.propTypes = ***REMOVED***
