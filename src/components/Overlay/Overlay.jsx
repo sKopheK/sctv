eff2d2c8605140167f58c34c327d4a717d2bbd0f
skophek <skophek@lipe.cz>
2021-05-05 12:45:32 +0100

    animated sc logo when screen is off

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -6,7 +6,15 @@ import './Overlay.scss';
 const Overlay = (***REMOVED*** className ***REMOVED***) => ***REMOVED***
   const ***REMOVED*** screenOn, hasSignal ***REMOVED*** = useContext(AppCtx);
   return useMemo(() => (
-    <div className=***REMOVED***`Overlay $***REMOVED***screenOn && !hasSignal ? 'no-signal' : ''***REMOVED*** $***REMOVED***className ?? ''***REMOVED***`***REMOVED*** />
+    <div className=***REMOVED***`Overlay $***REMOVED***screenOn && !hasSignal ? 'no-signal' : ''***REMOVED*** $***REMOVED***className ?? ''***REMOVED***`***REMOVED***>
+      ***REMOVED***screenOn ? '' : (
+        <h1 className="app-logo">
+          <span className="sc-logo"><span className="visually-hidden">StarCraft</span></span>
+          ***REMOVED***' '***REMOVED***
+          <span className="tv-ico"><span className="visually-hidden">TV</span></span>
+        </h1>
+      )***REMOVED***
+    </div>
   ), [screenOn, hasSignal, className]);
 ***REMOVED***;
 
