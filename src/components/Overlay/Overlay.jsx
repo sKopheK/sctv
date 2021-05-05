29fc6ff7e2f2b694e700489541ef6c9c0dc8209f
skophek <skophek@lipe.cz>
2021-05-05 12:53:25 +0100

    init logo animation only on first visit

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,21 +1,33 @@
 import PropTypes from 'prop-types';
-import React, ***REMOVED*** useContext, useMemo ***REMOVED*** from 'react';
+import React, ***REMOVED***
+  useContext, useEffect, useMemo, useState,
+***REMOVED*** from 'react';
 import AppCtx from '../../state/AppCtx';
 import './Overlay.scss';
 
+const firstVisitCssClass = 'first-run';
+
 const Overlay = (***REMOVED*** className ***REMOVED***) => ***REMOVED***
   const ***REMOVED*** screenOn, hasSignal ***REMOVED*** = useContext(AppCtx);
+  const [logoClass, setLogoClass] = useState(firstVisitCssClass);
+
+  useEffect(() => ***REMOVED***
+    if (screenOn) ***REMOVED***
+      setLogoClass(false);
+  ***REMOVED***
+***REMOVED***, [screenOn]);
+
   return useMemo(() => (
     <div className=***REMOVED***`Overlay $***REMOVED***screenOn && !hasSignal ? 'no-signal' : ''***REMOVED*** $***REMOVED***className ?? ''***REMOVED***`***REMOVED***>
       ***REMOVED***screenOn ? '' : (
         <h1 className="app-logo">
-          <span className="sc-logo"><span className="visually-hidden">StarCraft</span></span>
+          <span className=***REMOVED***`sc-logo $***REMOVED***logoClass***REMOVED***`***REMOVED***><span className="visually-hidden">StarCraft</span></span>
           ***REMOVED***' '***REMOVED***
-          <span className="tv-ico"><span className="visually-hidden">TV</span></span>
+          <span className=***REMOVED***`tv-ico $***REMOVED***logoClass***REMOVED***`***REMOVED***><span className="visually-hidden">TV</span></span>
         </h1>
       )***REMOVED***
     </div>
-  ), [screenOn, hasSignal, className]);
+  ), [screenOn, hasSignal, className, logoClass]);
 ***REMOVED***;
 
 Overlay.propTypes = ***REMOVED***
