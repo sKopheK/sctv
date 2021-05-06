c028f75dff81c8f5f08acbb71a9bc6b465cb4fe4
skophek <skophek@lipe.cz>
2021-05-06 19:09:56 +0100

    storage fallback value

--- src/storage.js
+++ src/storage.js
@@ -1,2 +1,8 @@
-export const getStoredData = (key) => localStorage.getItem(key);
+export const getStoredData = (key, fallback) => ***REMOVED***
+  const stored = localStorage.getItem(key);
+  if (stored === null && fallback !== undefined) ***REMOVED***
+    return fallback;
+***REMOVED***
+  return stored;
+***REMOVED***;
 export const storeData = (key, value) => localStorage.setItem(key, value);
