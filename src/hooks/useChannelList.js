4163119073906abc7819b5f239276240d06ba6ef
skophek <skophek@lipe.cz>
2021-10-04 22:52:04 -0500

    ChannelList - visualisation of full range

--- src/hooks/useChannelList.js
+++ src/hooks/useChannelList.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import ***REMOVED*** useCallback, useRef, useState ***REMOVED*** from 'react';
+import ***REMOVED*** CHANNEL_ID_MAX, CHANNEL_ID_MIN ***REMOVED*** from '../settings';
 
 const useChannelList = () => ***REMOVED***
   const request = useRef(null);
@@ -16,18 +17,27 @@ const useChannelList = () => ***REMOVED***
   ***REMOVED*** catch (error) ***REMOVED***
       console.error(error);
   ***REMOVED***
-    return ***REMOVED******REMOVED***;
+    return [];
 ***REMOVED***;
   const [***REMOVED*** visible, list ***REMOVED***, setState] = useState(***REMOVED*** visible: false, list: null ***REMOVED***);
 
   const get = async () => ***REMOVED***
     if (list === null) ***REMOVED***
       const ***REMOVED*** data ***REMOVED*** = await fetch();
+      const fullList = Array(CHANNEL_ID_MAX - CHANNEL_ID_MIN + 1)
+        .fill(null)
+        .map((_, id) => (***REMOVED*** id: id + CHANNEL_ID_MIN ***REMOVED***));
+      if (data) ***REMOVED***
+        data.forEach((channel) => ***REMOVED***
+          const channelIndex = fullList.findIndex((placeholder) => placeholder.id === channel.id);
+          fullList[channelIndex] = channel;
+      ***REMOVED***);
+    ***REMOVED***
       setState((oldState) => (***REMOVED***
         ...oldState,
-        list: data,
+        list: fullList,
     ***REMOVED***));
-      return data;
+      return fullList;
   ***REMOVED***
     return list;
 ***REMOVED***;
