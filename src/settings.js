e305dd840081a4507d104f7632495ef2b26a537a
skophek <skophek@lipe.cz>
2021-10-25 22:11:10 -0500

    ChannelList - correct range around active channel displayed; - fades out after a while

--- src/settings.js
+++ src/settings.js
@@ -4,3 +4,4 @@ export const VOLUME_CHANGE_STEP = 1;
 export const REMOTE_HIDE_TIMEOUT = 5000;
 export const CHANNEL_ID_MIN = 1;
 export const CHANNEL_ID_MAX = 9;
+export const CHANNEL_LIST_TIMEOUT = 5000;
